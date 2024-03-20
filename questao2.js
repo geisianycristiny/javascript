f1fd4abd3f430979f9c7a1a2f4b181e61a15bd63
Ana Eloize <anaeloizea@gmail.com>
2024-03-20 10:49:25 -0400

    questao2 finalizada

---
+++ questao2.js
@@ -0,0 +1,10 @@
+/*
+Questão 2: Como você pode verificar o tipo de uma variável em JavaScript? 
+*/
+
+let nome = "geisiany"
+let sobrenome = "Timóteo"
+let resultado;
+
+resultado = nome +" "+ sobrenome;
+console.log(resultado);
