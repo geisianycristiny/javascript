c6a0b2d87957672651247de0997d2cd2be6231b0
Ana Eloize <anaeloizea@gmail.com>
2024-03-25 09:34:36 -0400

    finalizado questao 15

---
+++ questao15.js
@@ -0,0 +1,9 @@
+/*
+Questão 15: Como você pode obter o comprimento de uma string em 
+JavaScript?
+*/
+
+let minhaString = "Olá Mundo!";
+let comprimentoDaString = minhaString.length;
+
+console.log(comprimentoDaString); // Output: 10
