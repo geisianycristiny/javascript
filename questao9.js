708044633a1340a79a5072156757716567c9f9f9
Ana Eloize <anaeloizea@gmail.com>
2024-03-25 09:32:22 -0400

    finalizado questao 9

---
+++ questao9.js
@@ -0,0 +1,8 @@
+/*
+Questão 9: Como você pode adicionar um novo elemento ao final de um array 
+em JavaScript?
+*/
+
+let array = [1, 2, 3, 4];
+array.push(5);
+console.log(array); // Saída: [1, 2, 3, 4, 5]
