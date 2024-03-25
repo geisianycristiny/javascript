3b3891f35b7cb92dfdcbd8774b091deb179246e4
Ana Eloize <anaeloizea@gmail.com>
2024-03-25 09:33:42 -0400

    finalizado questao 12

---
+++ questao12.js
@@ -0,0 +1,10 @@
+/*
+Questão 12: Como você pode iterar por meio de um array em JavaScript 
+usando um loop for?
+*/
+
+let array = [1, 2, 3, 4, 5];
+
+for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+}
