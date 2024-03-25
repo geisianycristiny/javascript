19878373bd6623f253c7efad5c2d4491b72a8bc1
Ana Eloize <anaeloizea@gmail.com>
2024-03-25 09:45:28 -0400

    finalizado questao 7

---
+++ questao7.js
@@ -0,0 +1,8 @@
+/*
+Questão 7: Como você pode declarar e atribuir um valor a uma variável em 
+uma única linha em JavaScript?
+*/
+
+let minhaVariavel = 10;
+const minhaConstante = 5;
+var minhaOutraVariavel = "Hello";
