54a8e6df59d3cffe113206ceb1b1e9061964c1e9
Ana Eloize <anaeloizea@gmail.com>
2024-03-25 09:35:21 -0400

    finalizado questao 18

---
+++ questao18.js
@@ -0,0 +1,12 @@
+/*
+Questão 18: Como você pode acessar o valor de uma propriedade de um 
+objeto em JavaScript?
+*/
+
+let pessoa = {
+    nome: "João",
+    idade: 30
+};
+
+console.log(pessoa["nome"]); // Output: "João"
+console.log(pessoa["idade"]); // Output: 30
