663c7676b370defe83897ac41876035a78b3e4a1
Ana Eloize <anaeloizea@gmail.com>
2024-03-25 09:34:01 -0400

    finalizado questao 13

---
+++ questao13.js
@@ -0,0 +1,9 @@
+/*
+Questão 13: Como você pode verificar se um valor está contido em um array 
+em JavaScript?
+*/
+
+let array = [1, 2, 3, 4, 5];
+
+console.log(array.includes(3)); // Output: true
+console.log(array.includes(6)); // Output: false
