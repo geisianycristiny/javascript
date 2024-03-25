71e5e0bd9a4cfaa0f16acb16d7f77d5a873043d1
Ana Eloize <anaeloizea@gmail.com>
2024-03-25 09:38:59 -0400

    finalizado questao 28

---
+++ questao28.js
@@ -0,0 +1,9 @@
+/*
+Questão 28: Como você pode converter uma string em um número inteiro em 
+JavaScript?
+*/
+
+let stringHexadecimal = "1A"; // Representação hexadecimal de 26
+let numeroInteiro = parseInt(stringHexadecimal, 16);
+
+console.log(numeroInteiro); // Output: 26
