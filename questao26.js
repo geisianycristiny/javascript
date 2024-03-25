477d19e1bf508074d6ac92913b821d6f398537dd
Ana Eloize <anaeloizea@gmail.com>
2024-03-25 09:38:29 -0400

    finalizado questao 26

---
+++ questao26.js
@@ -0,0 +1,8 @@
+/*
+Questão 26: Como você pode usar o operador ternário em JavaScript?
+*/
+
+let idade = 20;
+let status = (idade >= 18) ? 'adulto' : 'menor de idade';
+
+console.log(status); // Output: "adulto"
