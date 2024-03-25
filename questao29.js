2e925e89329b36ddc19739343c80cf8cf906e4e1
Ana Eloize <anaeloizea@gmail.com>
2024-03-25 09:39:15 -0400

    finalizado questao 29

---
+++ questao29.js
@@ -0,0 +1,9 @@
+/*
+Questão 29: Como você pode converter um número em uma string em
+JavaScript?
+*/
+
+let numero = 123;
+let stringNumero = String(numero);
+
+console.log(stringNumero); // Output: "123"
